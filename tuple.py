9e9e6bacd2cdae332243e79a879d879b9bd3aef3
iyanahmed1 <malumasaid123@gmail.com>
2024-09-03 02:34:56 +0300

    commit

--- tuple.py
+++ tuple.py
@@ -1,3 +1,7 @@
+# is an ordered sequence of items, same as a list
+# difference between tupple and list is that tuples are immutable
+# we use () to create a tuple eg products=("Xbox",499.99, "Playstation",399.99)
+# allows duplicates
 product=('microsoft','Xbox','499.99')
 print(product[0])
 print(product[2])
