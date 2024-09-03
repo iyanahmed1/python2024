eeb0aadb22150ca020bb2e3eb4661576dabc8e99
iyanahmed1 <malumasaid123@gmail.com>
2024-09-03 23:27:24 +0300

    commit

---
+++ netflixmovies.py
@@ -0,0 +1,17 @@
+# {1: 3, 7: 1, 3: 3, 5: 2, 2: 6, 9: 2}
+movies = [
+    {"title": "The Shawshank Redemption", "genre": "Drama", "rating": 9.3},
+    {"title": "The Godfather", "genre": "Crime", "rating": 9.2},
+    {"title": "Pulp Fiction", "genre": "Crime", "rating": 8.9},
+    {"title": "Fight Club", "genre": "Drama", "rating": 8.8},
+    {"title": "The Dark Knight", "genre": "Action", "rating": 9.0},
+]
+#extract movies from specific genre
+Crime=[movie for movie in movies if movie['genre']=='Crime']
+print('Crime movies\n')
+for movie in Crime:
+    print(f"{movie['title']}:{movie['genre']}")
+#extract all the genres
+genres=[movie['genre'] for movie in movies]
+genre_count={genre:genres.count(genre) for genre in genres }
+print(genre_count)
