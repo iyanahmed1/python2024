4e805caa976989a08293ed7012e2346cae01c361
iyanahmed1 <malumasaid123@gmail.com>
2024-09-13 19:48:24 +0300

    Create input.py

---
+++ input.py
@@ -0,0 +1,20 @@
+"""name= input('Please enter your name? ')
+colour= input('Please enter your favorite color? ')
+print(name + ' likes ' + colour)"""
+
+"""birth_year=input('Please enter your birth year: ')
+age = 2024 - int(birth_year)
+print(age)"""
+
+"""weight_pounds= input('Please enter your weight in pounds: ')
+weight_kg=int(weight_pounds)*0.45
+print(weight_kg)"""
+
+"""name='jeniffer'
+print(name[1:-1])"""
+
+first_name='Hayyan'
+last_name='Mohamed'
+print(f'{first_name} [{last_name}] is a coder')
+
+
